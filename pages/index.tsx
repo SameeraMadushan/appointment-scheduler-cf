867e57e6b456b5a8cc6963e571bfd5a1b0c69093
Sameera Madushan <sameeradelpechithra@gmail.com>
2022-08-16 21:45:03 +0530

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className="container mx-auto flex justify-center align-middle h-full">
+      <h1 className="text-6xl font-semibold italic">
+        Next.js Tailwind CSS check
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
